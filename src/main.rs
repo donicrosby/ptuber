c242a38db4dc05d08a515436a4df624d439ebbab
donicrosby <donicrosby1995@gmail.com>
2023-01-08 21:47:27 -0500

    feat: updated binary to output stdout/stderr on windows

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-#![windows_subsystem = "windows"]
-
+use cfg_if::cfg_if;
 use log::error;
 use ptuber::{PTuber, PtuberResult as Result};
+#[cfg(windows)]
+use windows::Win32::{
+    System::Console::{AttachConsole, FreeConsole, ATTACH_PARENT_PROCESS}
+};
+
+cfg_if! {
+    if #[cfg(windows)] {
+        fn attach_to_console() {
+            unsafe {
+                FreeConsole();
+                AttachConsole(ATTACH_PARENT_PROCESS);
+            }
+        }
+    }
+    
+}
+
+
 
 fn main() -> Result<()> {
+    cfg_if::cfg_if!{
+        if #[cfg(windows)] {
+            attach_to_console();
+        }
+    }
     env_logger::init();
     match PTuber::new() {
         Ok(mut ptuber) => {
